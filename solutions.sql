f3455447e4fa6cc915a5f6f06ba3e18fd2da5880
Charan <charanmanubolu@gmail.com>
2026-03-15 22:48:00 +0530

    highest spend month and amount spent in that month for each card type

--- solutions.sql
+++ solutions.sql
@@ -1,8 +1,5 @@
 select * from credit_card_transcations;
 
-select distinct city from credit_card_transcations;
-select distinct card_type from credit_card_transcations;
-select distinct exp_type from credit_card_transcations;
 /*
 write a query to print top 5 cities with highest spends and their 
 percentage contribution of total credit card spends 
@@ -30,3 +27,31 @@ FROM ranked_cities
 WHERE spend_rank <= 5;
 
 
+
+-- write a query to print highest spend month and amount spent in that month for each card type
+
+WITH MonthlyCardTotals AS (
+    SELECT
+        card_type,
+        DATEPART(YEAR, transaction_date) AS transaction_year,
+        DATEPART(MONTH, transaction_date) AS transaction_month,
+        SUM(amount) AS total_amount
+    FROM credit_card_transcations
+    GROUP BY 
+        card_type, 
+        DATEPART(YEAR, transaction_date), 
+        DATEPART(MONTH, transaction_date)
+),
+RankedMonthlyTotals AS (
+    SELECT
+      *,
+        RANK() OVER(PARTITION BY card_type ORDER BY total_amount DESC) AS amount_rank
+    FROM MonthlyCardTotals
+)
+SELECT 
+    card_type,
+    transaction_year,
+    transaction_month,
+    total_amount
+FROM RankedMonthlyTotals
+WHERE amount_rank = 1;
