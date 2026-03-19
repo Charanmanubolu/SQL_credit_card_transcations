1a582ad211ffcdd8ad53e44f50ed35f992dc5371
Charan <charanmanubolu@gmail.com>
2026-03-19 18:10:19 +0530

    Final commit

--- solutions.sql
+++ solutions.sql
@@ -121,4 +121,74 @@ FROM RankedCityExpenses
 GROUP BY city
 ORDER BY city;
 
+--write a query to find percentage contribution of spends by females for each expense type
 
+SELECT 
+    exp_type,
+    (SUM(CASE WHEN gender = 'F' THEN amount ELSE 0 END) * 1.0 / SUM(amount)) * 100 AS female_spend_percentage
+FROM credit_card_transcations
+GROUP BY exp_type
+ORDER BY female_spend_percentage DESC;
+
+--which card and expense type combination saw highest month over month growth in Jan-2014
+
+
+WITH MonthlySpends AS (
+    SELECT 
+        card_type,
+        exp_type,
+        DATEPART(YEAR, transaction_date) AS transaction_year, 
+        DATEPART(MONTH, transaction_date) AS transaction_month,
+        SUM(amount) AS current_month_amount
+    FROM credit_card_transcations
+    GROUP BY 
+        card_type,
+        exp_type,
+        DATEPART(YEAR, transaction_date), 
+        DATEPART(MONTH, transaction_date)
+), 
+SpendsWithPreviousMonth AS (
+    SELECT 
+        card_type,
+        exp_type,
+        transaction_year,
+        transaction_month,
+        current_month_amount,
+        LAG(current_month_amount) OVER(PARTITION BY card_type, exp_type ORDER BY transaction_year, transaction_month) AS previous_month_amount
+    FROM MonthlySpends
+)
+SELECT TOP 1 
+    card_type,
+    exp_type,
+    current_month_amount,
+    previous_month_amount,
+    (current_month_amount - previous_month_amount) AS mom_growth_amount
+FROM SpendsWithPreviousMonth
+WHERE transaction_year = 2014 AND transaction_month = 1
+ORDER BY mom_growth_amount DESC;
+
+
+-- during weekends which city has highest total spend to total no of transcations ratio 
+
+select top 1 city,sum(amount)*1.0/ count(*) as ratio
+from credit_card_transcations
+where DATEPART(WEEKDAY,transaction_date) in (1,7)
+group by city
+order by ratio desc
+
+--which city took least number of days to reach its 500th transaction after the first transaction in that city
+
+with datee as (
+select city,transaction_date,
+ROW_NUMBER() over(partition by city order by transaction_date) as row_no
+from credit_card_transcations
+)
+, min_max_date as (select city,
+min(case when row_no=1 then transaction_date end ) as min_date,
+max(case when row_no=500 then transaction_date end ) as max_date
+from datee 
+group by city
+having max(case when row_no=500 then transaction_date end ) is not null )
+select top 1  city, DATEDIFF(day,min_date,max_date) as between_dates
+from min_max_date
+order by between_dates 
